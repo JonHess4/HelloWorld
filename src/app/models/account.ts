34fb1cd2e295a895128d8592a5b0260a3786d860
Hess <jonathan.hess@accenturefederal.com>
2019-11-21 15:32:39 -0500

    progress on simple login

--- src/app/models/account.ts
+++ src/app/models/account.ts
@@ -1,13 +1,13 @@
-export class User {
+export class Account {
     id: number;
-    email: string;
+    username: string;
     password: string
     role: string;
     status: string;
 
-    constructor(id: number, email: string, password: string, role: string, status: string) {
+    constructor(id: number, username: string, password: string, role: string, status: string) {
         this.id = id;
-        this.email = email;
+        this.username = username;
         this.password = password;
         this.role = role;
         this.status = status;
--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -3,38 +3,38 @@ import { Observable } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 
-import { User } from '../models/user';
+import { Account } from '../models/account';
 import { EndpointsService } from './endpoints.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
-
-  loggedUser: User;
+  private headers = new HttpHeaders({'Content-Type': 'application/json'});
+  loggedUser: Account;
 
   constructor(
     private http: HttpClient,
     private endpointsService: EndpointsService
   ) { }
 
-  login(email: string, password: string): Observable<User> {
+  login(email: string, password: string): Observable<Account> {
 
     const url: string = this.endpointsService.LOGIN;
-    const body = JSON.stringify(new User(null, email, password, null, null));
+    const body = JSON.stringify(new Account(null, email, password, null, null));
 
-    return this.http.post(url, body, { withCredentials: true }).pipe(
-      map(resp => resp as User)
+    return this.http.post(url, body, {headers: this.headers, withCredentials: true }).pipe(
+      map(resp => resp as Account)
     );
   }
 
-  register(email: string, password: string): Observable<User> {
+  register(email: string, password: string): Observable<Account> {
 
     const url: string = this.endpointsService.REGISTER;
-    const body = JSON.stringify(new User(null, email, password, null, null));
+    const body = JSON.stringify(new Account(null, email, password, null, null));
 
-    return this.http.post(url, body, { withCredentials: true }).pipe(
-      map(resp => resp as User)
+    return this.http.post(url, body, {headers: this.headers, withCredentials: true }).pipe(
+      map(resp => resp as Account)
     );
 
   }
